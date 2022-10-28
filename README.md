26e7e998bcdfe204ee4c4dee5ca898054e55b207
Nick Jensen <nickspoon@gmail.com>
2022-10-28 21:39:03 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### Code Validator
+
+A demo site to test out netlify functions.
+
+Using `netlify-cli`, run a live-server `netlify dev`, then browse to
+[http://localhost:8888/?code=abc123](http://localhost:8888/?code=abc123)
---
+++ netlify/functions/validate.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { Handler } from '@netlify/functions';
+
+const processFile = async () => {
+   const records = [];
+   const parser = fs
+      .createReadStream(`assets/codes.csv`)
+      .pipe(parse({
+         // CSV options if any
+      }));
+   for await (const record of parser) {
+      // Work with each record
+      records.push(record);
+   }
+   return records;
+};
+
+const handler: Handler = async (event) => {
+   const code = event.queryStringParameters.code;
+
+   const records = await processFile();
+   console.info(records);
+
+   const isValid = records.find(rec => rec[0] === code);
+
+   return {
+      statusCode: 200,
+      body: JSON.stringify({ isValid }),
+   };
+};
+
+export { handler };
